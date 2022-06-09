af7e7e0b028d8480aba09819b7ad6d7f818ba8d4
calderonsamuel <samuel.calderon@uarm.pe>
2022-06-09 16:12:41 +0000

    add DT options

---
+++ R/fun_DT.R
@@ -0,0 +1,18 @@
+options_DT <- function() {
+  list(
+    pageLength = 10,
+    lengthMenu = c(10, 25, 40),
+    language = list(
+      emptyTable = "Cliente no tiene registros en esta sección",
+      zeroRecords = "Cliente no existe. Buscar con otros datos o Agregar cliente",
+      infoEmpty = "Datos no coinciden con ningún registro.",
+      infoFiltered = "(filtrado de un total de _MAX_ registros)",
+      lengthMenu = "Mostrar _MENU_ registros",
+      info = "Mostrando _START_ al _END_ de _TOTAL_ registros",
+      search = "Buscar:",
+      paginate = list(
+        previous = "Anterior",
+        'next' = "Siguiente")
+    )
+  )
+}
