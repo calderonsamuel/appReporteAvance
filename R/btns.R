99065b7137e0ab038e02e141f68e0c1406a30f38
calderonsamuel <samuel.calderon@uarm.pe>
2022-06-10 17:09:45 +0000

    document buttons

--- R/btns.R
+++ R/btns.R
@@ -1,6 +1,12 @@
-
+#' Pretty action buttons factory
+#'
+#' @inheritParams shinyWidgets::actionBttn
+#'
+#' @return A function to generate action buttons with a particular styling
+#'
 btn_x <- function(color, label) {
-  function(inputId, icon = NULL, size = "md", block = FALSE, no_outline = TRUE) {
+  function(inputId, icon = NULL, size = "md", block = FALSE,
+           no_outline = TRUE) {
     shinyWidgets::actionBttn(
       inputId = inputId,
       label = label,
@@ -14,6 +20,21 @@ btn_x <- function(color, label) {
   }
 }
 
+#' Pre styled pretty action buttons
+#'
+#' These functions have predefined styling and icons for labels.
+#' The defaults are inherited from [shinyWidgets::actionBttn()].
+#'
+#' @inheritParams shinyWidgets::actionBttn
+#' @name buttons
+#' @return HTML code for pretty action buttons
+#' @seealso [shinyWidgets::actionBttn()]
+#'
+NULL
+#> NULL
+
+#' @rdname buttons
 btn_add <- btn_x(color = "success", label = icon("plus"))
 
+#' @rdname buttons
 btn_trash <- btn_x(color = "danger", label = icon("trash"))
