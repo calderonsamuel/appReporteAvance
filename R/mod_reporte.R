ffa13d252633def459b338c98991719ce499cf82
calderonsamuel <samuel.calderon@uarm.pe>
2022-06-10 19:42:19 +0000

    create tasks columns

--- R/mod_reporte.R
+++ R/mod_reporte.R
@@ -10,20 +10,54 @@
 mod_reporte_ui <- function(id){
   ns <- NS(id)
   tagList(
-    wellPanel(
-      prettyRadioButtons(
-        inputId = ns("estado"),
-        label = "Indique estado de avance",
-        choices = c("Pendiente", "En proceso", "Postergado/Pausado",
-                    "Esperando revision", "Terminado"),
-        selected = character()
+    fluidRow(
+      bs4Dash::box(
+        title = "Pendiente",
+        solidHeader = TRUE,
+        collapsible = FALSE,
+        width = 3
       ),
-      textAreaInput(
-        inputId = ns("descripcion_avance"),
-        label = "Ingrese descripción del avance",
-        placeholder = "Sea lo más detallado posible"
+      bs4Dash::box(
+        title = "En proceso",
+        solidHeader = TRUE,
+        collapsible = FALSE,
+        width = 3
+      ),
+      col_3(
+        bs4Dash::box(
+          title = "En revisión",
+          solidHeader = TRUE,
+          collapsible = FALSE,
+          width = 12
+        ),
+        bs4Dash::box(
+          title = "Pausado",
+          solidHeader = TRUE,
+          collapsible = FALSE,
+          width = 12
+        ),
+      ),
+      bs4Dash::box(
+        title = "Terminado",
+        solidHeader = TRUE,
+        collapsible = FALSE,
+        width = 3
       )
     )
+    # wellPanel(
+    #   prettyRadioButtons(
+    #     inputId = ns("estado"),
+    #     label = "Indique estado de avance",
+    #     choices = c("Pendiente", "En proceso", "Postergado/Pausado",
+    #                 "Esperando revision", "Terminado"),
+    #     selected = character()
+    #   ),
+    #   textAreaInput(
+    #     inputId = ns("descripcion_avance"),
+    #     label = "Ingrese descripción del avance",
+    #     placeholder = "Sea lo más detallado posible"
+    #   )
+    # )
   )
 }
 
@@ -35,6 +69,16 @@ mod_reporte_server <- function(id){
     ns <- session$ns
 
   })
+}
+
+mod_reporte_testapp <- function() {
+  ui <- fluidPage(mod_reporte_ui("reporte_1"))
+
+  server <- function(input, output, session) {
+    mod_reporte_server("reporte_1")
+  }
+
+  shinyApp(ui, server)
 }
 
 ## To be copied in the UI
