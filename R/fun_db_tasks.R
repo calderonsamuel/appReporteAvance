7de7732540918e6400e8ec86f3861978b37da548
calderonsamuel <samuel.calderon@uarm.pe>
2022-06-12 21:10:36 +0000

    allow updates in db

--- R/fun_db_tasks.R
+++ R/fun_db_tasks.R
@@ -24,18 +24,42 @@ get_tasks <- function() {
   return(data)
 }
 
-insert_task <- function(task_list) {
+insert_task <- function(task_list, with_print = TRUE) {
   con <- RSQLite::dbConnect(RSQLite::SQLite(), "reporte_avance.db")
   RSQLite::dbWriteTable(con, "tasks", task_list, append = TRUE)
   RSQLite::dbDisconnect(con)
-  print(sprintf("inserted task with id %s", task_list$task_id))
+  if(with_print) print(sprintf("inserted task with id %s", task_list$task_id))
 }
 
-delete_task <- function(task_id) {
+delete_task <- function(task_id, with_print = TRUE) {
   con <- RSQLite::dbConnect(RSQLite::SQLite(), "reporte_avance.db")
   RSQLite::dbExecute(con, sprintf("DELETE FROM tasks WHERE (task_id = '%s')", task_id))
   RSQLite::dbDisconnect(con)
-  print(sprintf("deleted task with id %s", task_id))
+  if(with_print) print(sprintf("deleted task with id %s", task_id))
+}
+
+get_task_from_id <- function(task_id) {
+  con <- RSQLite::dbConnect(RSQLite::SQLite(), "reporte_avance.db")
+  data <- RSQLite::dbGetQuery(con, sprintf("SELECT * FROM tasks WHERE (task_id = '%s')", task_id))
+  RSQLite::dbDisconnect(con)
+  return(data)
+}
+
+mdf_task_status <- function(task_id, new_status, with_print = TRUE) {
+  con <- RSQLite::dbConnect(RSQLite::SQLite(), "reporte_avance.db")
+  task <- get_task_from_id(task_id)
+  task$status <- new_status
+  delete_task(task_id, with_print = FALSE)
+  insert_task(task, with_print = FALSE)
+  RSQLite::dbDisconnect(con)
+  if(with_print) print(sprintf("modified task with id %s", task$task_id))
+}
+
+get_task_from_user <- function(user) {
+  con <- RSQLite::dbConnect(RSQLite::SQLite(), "reporte_avance.db")
+  data <- RSQLite::dbGetQuery(con, sprintf("SELECT * FROM tasks WHERE (user = '%s')", user))
+  RSQLite::dbDisconnect(con)
+  return(data)
 }
 
 # create_reporte_tasks()
