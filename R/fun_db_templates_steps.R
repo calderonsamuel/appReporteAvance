c917c1d9e82b122bfd8055a2c15c0287f26f0c2c
case-dgco <dgco93@mininter.gob.pe>
2022-06-20 16:39:24 +0000

    better str size for fields

--- R/fun_db_templates_steps.R
+++ R/fun_db_templates_steps.R
@@ -5,8 +5,8 @@ create_reporte_templates_steps <- function() {
     fields_list <- data.frame(
       user_id = strrep(" ", 64),
       template_id = strrep(" ", 64),
-      step_id = strrep(" ", 64),
-      step_description = strrep(" ", 64)
+      step_id = strrep(" ", 10),
+      step_description = strrep(" ", 256)
     )
 
     DBI::dbWriteTable(con, "templates_steps", fields_list)
@@ -29,7 +29,7 @@ insert_template_steps <- function(field_list, with_print = TRUE) {
   DBI::dbWriteTable(con, "templates_steps", field_list, append = TRUE)
   DBI::dbDisconnect(con)
   if (with_print) {
-    print(sprintf("inserted template steps with id %s", field_list$template_id))
+    print(sprintf("inserted steps %s in template with id %s", field_list$step_id, field_list$template_id))
   }
 }
 
@@ -43,3 +43,15 @@ delete_template_steps <- function(template_id, with_print = TRUE) {
 # create_reporte_templates_steps()
 # remove_table_from_reporte("templates_steps")
 #
+
+# readxl::read_excel("inst/data-raw/template_example.xlsx") |>
+#   janitor::clean_names() |>
+#   subset(unidad_de_organizacion == "DDC", select = c(no, descripcion_de_la_actividad)) |>
+#   setNames(c("step_id", "step_description")) |>
+#   dplyr::mutate(
+#     step_id = sprintf("step_%02i", step_id),
+#     user_id = "dgco93@mininter.gob.pe",
+#     template_id = "M02.01.01.00.02"
+#   ) |>
+#   insert_template_steps()
+
