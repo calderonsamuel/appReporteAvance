f2caf18db7264291e1da753bf800dbb316c9726f
case-dgco <dgco93@mininter.gob.pe>
2022-06-20 17:09:32 +0000

    use boxSidebar

--- R/mod_tasks.R
+++ R/mod_tasks.R
@@ -11,27 +11,15 @@ mod_tasks_ui <- function(id){
   ns <- NS(id)
   tagList(
     bs4Dash::box(
-      collapsible = FALSE, width = 12,
-      fluidRow(
-        tags$span(
-          shinyWidgets::dropdownButton(
-            icon = icon("plus"), status = "success", inline = TRUE, circle = TRUE,
-            tooltip = shinyWidgets::tooltipOptions(title = "Nueva tarea"),
-
-            selectInput(
-              ns("user"), "Seleccione encargado",
-              choices = with(data = get_users(),
-                             expr = setNames(object = user_id,
-                                             nm = paste(name, last_name)))
-            ),
-            textAreaInput(ns("description"), "Descripción de tarea"),
-            btn_add(ns("add_task"))
-          ),
-          btn_trash(ns("delete_task"))
-        )
+      title = "Tareas actuales",
+      width = 12,
+      sidebar = bs4Dash::boxSidebar(
+        id = "sidebar",
+        width = 25,
+        btn_add(ns("add")),
+        btn_trash(ns("remove"))
       ),
-      fluidRow(DT::DTOutput(ns("tabla")))
-
+      DT::DTOutput(ns("tabla"))
     )
   )
 }
@@ -58,67 +46,94 @@ mod_tasks_server <- function(id, user_iniciado){
       )
     })
 
+    task_for_deleting <- reactive(vals$data_tasks$task_id[input$tabla_rows_selected])
+
+    observeEvent(input$add, {
+      showModal(modalDialog(
+        title = "Nueva tarea",
+
+        selectInput(
+          inputId = ns("user"),
+          label = "Seleccione encargado",
+          choices = with(data = get_users(),
+                         expr = setNames(object = user_id,
+                                         nm = paste(name, last_name)))
+        ),
+        textAreaInput(ns("description"), "Descripción de tarea"),
+
+        footer = tagList(
+          modalButton("Cancelar"),
+          btn_agregar(ns("save"))
+        )
+      ))
+    })
+
+    observeEvent(input$save, {
+
+      if (input$description == "") {
+        alert_error(session, "Debe añadir una descripción")
+      } else {
+        insert_task(new_task_data())
+        vals$data_tasks <- get_tasks()
+        # updateSelectInput(session, "user", choices = get_users()$user_id)
+        updateTextAreaInput(session, "description", value = "")
+
+        removeModal()
+
+        alert_success(session = session, text = "La tarea se añadió correctamente")
+      }
+    })
+
+    observeEvent(input$remove,{
+
+      if (length(task_for_deleting()) == 0) {
+        alert_error(session, "Debe seleccionar una tarea a eliminar")
+      } else {
+        delete_task(task_for_deleting())
+        vals$data_tasks <- get_tasks()
+        alert_info(session, "Tarea eliminada")
+      }
+
+    })
+
+
     output$tabla <- DT::renderDT(
       expr = vals$data_tasks,
       options = options_DT(),
       selection = 'single'
     )
 
-    task_for_deleting <- reactive(vals$data_tasks$task_id[input$tabla_rows_selected])
-
-    output$ids_selected <- renderPrint(task_for_deleting())
-
-    observeEvent(input$add_task, {
-
-      if (input$description == "") {
-        alert_error(session, "Debe añadir una descripción")
-      } else {
-        insert_task(new_task_data())
-
-        vals$data_tasks <- get_tasks()
-
-        # updateSelectInput(session, "user", choices = get_users()$user_id)
-        updateTextAreaInput(session, "description", value = "")
-
-        alert_success(session = session, text = "La tarea se añadió correctamente")
-      }
-    })
-
-    observeEvent(input$delete_task,{
-
-      if (length(task_for_deleting()) == 0) {
-
-        alert_error(session, "Debe seleccionar una tarea a eliminar")
-
-      } else {
-        delete_task(task_for_deleting())
-
-        vals$data_tasks <- get_tasks()
-
-        alert_info(session, "Tarea eliminada")
-      }
-
-    })
-
   })
 }
 
-
-mod_tasks_testapp <- function() {
-  ui <- fluidPage(
+mod_tasks_testapp <- function(id = "test") {
+  ui <- tagList(
     tags$head(
       shinyWidgets::useSweetAlert()
     ),
-    mod_tasks_ui("tasks_1")
+    bs4Dash::dashboardPage(
+      header = bs4Dash::dashboardHeader(title = "TEST"),
+      sidebar = bs4Dash::dashboardSidebar(
+        bs4Dash::sidebarMenu(
+          bs4Dash::menuItem(text = "Tareas",
+                            tabName = "tasks",
+                            icon = icon("calendar-plus"))
+        )
+      ),
+      body = bs4Dash::dashboardBody(
+        bs4Dash::tabItem(tabName = "tasks", mod_tasks_ui(id))
+      )
+    )
   )
 
   server <- function(input, output, session) {
     user_iniciado <- reactive("dgco93@mininter.gob.pe")
-    mod_tasks_server("tasks_1", user_iniciado)
+    mod_tasks_server(id, user_iniciado)
   }
 
   shinyApp(ui, server)
 }
+
 
 ## To be copied in the UI
 # mod_tasks_ui("tasks_1")
