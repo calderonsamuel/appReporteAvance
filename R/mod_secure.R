5e217dc89f4e561975706e0fb8a265a6e9503ed7
Samuel Calderon <samuel.calderon@uarm.pe>
2022-07-04 15:45:43 -0500

    fix broken reactive

--- R/mod_secure.R
+++ R/mod_secure.R
@@ -90,12 +90,12 @@ mod_secure_server <- function(id, user_iniciado){
   })
 }
 
-mod_secure_testapp <- function(id = "test", privileges = "admin", user_iniciado = "samuelcs8.17@gmail.com") {
+mod_secure_testapp <- function(id = "test", privileges = "admin") {
 
   ui <- mod_secure_ui(id, privileges = privileges)
 
   server <- function(input, output, session) {
-    user_iniciado <- reactive(user_iniciado)
+    user_iniciado <- reactive("samuelcs8.17@gmail.com")
     mod_secure_server(id, user_iniciado)
   }
 
