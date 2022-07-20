e32191ea66015a327165dcd8f543f6d9859719ae
Samuel Calderon <samuel.calderon@uarm.pe>
2022-07-20 17:09:28 -0500

    try #4

--- R/mod_secure.R
+++ R/mod_secure.R
@@ -12,12 +12,21 @@ mod_secure_ui <- function(id, privileges){
   tagList(
     bs4Dash::dashboardPage(
         # preloader = list(html = tagList(waiter::spin_pixel(), "Cargando ..."), color = "#3c8dbc"),
-      bs4Dash::dashboardHeader(title = "Reporte"),
+      bs4Dash::dashboardHeader(
+          title = "Reporte",
+          rightUi = tagList(
+              bs4Dash::dropdownMenu(
+                  icon = icon("gear"),
+                  type = "messages",
+                  bs4Dash::notificationItem(inputId = ns("refresh"), text = icon("refresh"), status = "primary")
+              )
+          )
+      ),
       bs4Dash::dashboardSidebar(
         collapsed = TRUE,
         expandOnHover = FALSE,
         bs4Dash::sidebarMenu(
-          id = "sidebar",
+          id = ns("sidebar"),
           bs4Dash::menuItem(
             text = "Reporte de avance",
             tabName = "progress",
@@ -28,7 +37,7 @@ mod_secure_ui <- function(id, privileges){
           bs4Dash::menuItem(
               text = "Plantillas",
               icon = icon("book"),
-              tabName = "admin_templates"
+              tabName = "templates"
           ),
           if (privileges == "admin") {
             tagList(
@@ -61,8 +70,8 @@ mod_secure_ui <- function(id, privileges){
             mod_users_ui(ns("admin_users_1"))
           ),
           bs4Dash::tabItem(
-            tabName = "admin_templates",
-            mod_templates_ui(ns("admin_templates_1"))
+            tabName = "templates",
+            mod_templates_ui(ns("templates_1"))
           ),
           bs4Dash::tabItem(
             tabName = "admin_groups",
@@ -80,6 +89,7 @@ mod_secure_ui <- function(id, privileges){
 mod_secure_server <- function(id, user_iniciado){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
+    privileges <- user_get_privileges(user_iniciado)
 
     if (is.null(user_iniciado))  {
         message("No se ha definido 'user_iniciado()' en 'mod_secure_server'")
@@ -89,9 +99,17 @@ mod_secure_server <- function(id, user_iniciado){
 
     mod_progress_server("progress_1", user_iniciado)
     mod_tasks_server("tasks_1", user_iniciado)
-    mod_users_server("admin_users_1")
-    mod_templates_server("admin_templates_1", user_iniciado)
-    mod_groups_server("admin_groups_1", user_iniciado)
+    mod_templates_server("templates_1", user_iniciado)
+
+    if (privileges == "admin") {
+        mod_users_server("admin_users_1")
+        mod_groups_server("admin_groups_1", user_iniciado)
+    }
+
+    observeEvent(input$refresh, {
+        session$reload()
+        bs4Dash::updateTabItems(session = session, inputId = "sidebar", selected = "progress")
+    })
 
   })
 }
