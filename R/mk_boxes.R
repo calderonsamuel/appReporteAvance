b7fee46231db36c5a1ac247e39bf82a824d2eac7
calderonsamuel <samuel.calderon@uarm.pe>
2022-07-19 20:16:00 -0500

    add helper functions

--- R/mk_boxes.R
+++ R/mk_boxes.R
@@ -1,37 +1,56 @@
 mk_box <- function(icon = NULL, background = NULL) {
-  function(task, ..., label = NULL, dropdownMenu = NULL, ns = NULL) {
-      if (!isTruthy(task)) return(NULL)
-        id <- if (is.null(ns)) task$task_id else ns(task$task_id)
+    function(task, ..., label = NULL, dropdownMenu = NULL, ns = NULL) {
+        if (!isTruthy(task)) return(NULL)
+
+        task_id_ns <- ns_safe(task$task_id, ns)
         background <- if (task_is_from_group(task$task_id)) "olive" else background
-    bs4Dash::box(
-      title = task$task_description,
-      h6(paste0("Responsable: ", task$assignee$user_name)),
-      h6(paste0("Asignado por: ", task$reviewer$user_name)),
-      h6(paste0("Plantilla: ", task$template$template_description)),
-      dropdownMenu = bs4Dash::boxDropdown(
-          bs4Dash::boxDropdownItem("Modificar", id = id)
-      ),
-      ...,
-      footer = NULL,
-      status = NULL,
-      solidHeader = FALSE,
-      background = background,
-      width = 12, # default is 6´
-      height = NULL,
-      collapsible = TRUE,
-      collapsed = TRUE, # default is FALSE
-      closable = FALSE,
-      maximizable = FALSE,
-      icon = icon,
-      gradient = FALSE,
-      boxToolSize = "sm",
-      elevation = NULL,
-      headerBorder = TRUE,
-      label = label,
-      sidebar = NULL,
-      id = NULL
+        bs4Dash::box(
+            title = task$task_description,
+            dropdownMenu = bs4Dash::boxDropdown(
+              bs4Dash::boxDropdownItem("Modificar", id = task_id_ns)
+            ),
+            box_interior(
+                assignee_name = task$assignee$user_name,
+                reviewer_name = task$reviewer$user_name,
+                template_description = task$template$template_description
+            ),
+            ...,
+            footer = NULL,
+            status = NULL,
+            solidHeader = FALSE,
+            background = background,
+            width = 12, # default is 6´
+            height = NULL,
+            collapsible = TRUE,
+            collapsed = TRUE, # default is FALSE
+            closable = FALSE,
+            maximizable = FALSE,
+            icon = icon,
+            gradient = FALSE,
+            boxToolSize = "sm",
+            elevation = NULL,
+            headerBorder = TRUE,
+            label = label,
+            sidebar = NULL,
+            id = NULL
+        )
+    }
+}
+
+box_bg_by_assignee <- function(task_id) {
+
+}
+
+ns_safe <- function(id, ns = NULL) {
+    if (is.null(ns)) id else ns(id)
+}
+
+box_interior <- function(assignee_name, reviewer_name, template_description){
+    tagList(
+        h6(glue::glue("Responsable: {assignee_name}")),
+        h6(glue::glue("Asignado por: {reviewer_name}")),
+        h6(glue::glue("Plantilla: {template_description}"))
     )
-  }
 }
 
 box_pendientes <- mk_box(icon = icon("calendar"))
