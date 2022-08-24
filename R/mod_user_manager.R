4119d09425e4874faf4e61c4abcafba9186dca47
Samuel Calderon <samuel.calderon@uarm.pe>
2022-08-24 14:44:04 -0500

    add mod_user_manager

---
+++ R/mod_user_manager.R
@@ -0,0 +1,138 @@
+#' user_manager UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd
+#'
+#' @importFrom shiny NS tagList
+mod_user_manager_ui <- function(id, user_iniciado) {
+    ns <- NS(id)
+    tagList(
+        btn_agregar(ns("add"), icon = icon("plus"))
+    )
+}
+
+mod_user_manager_output <- function(id, user_iniciado) {
+    ns <- NS(id)
+    tagList(
+        bs4Dash::box(
+            title = "Gestión de usuario",
+            id = ns("box_manager"),
+            width = 12,
+
+            fluidRow(
+                col_4(textInput(ns("user_id"), "ID")),
+                col_4(textInput(ns("name"), "Nombres")),
+                col_4(textInput(ns("last_name"), "Apellidos"))
+            ),
+            fluidRow(
+                col_4(selectInput(ns("privileges"), "Privilegios", choices = c("user1", "user2", "admin"))),
+                col_4(selectInput(ns("responds_to"), "Responde a:", choices = user_get_from_privileges("user2"))),
+                col_4(dateInput(ns("date_added"), "Fecha", language = "es", value = lubridate::today("America/Lima")))
+            ),
+            btn_cancelar(ns("cancelar")),
+            btn_guardar(ns("save"))
+        )
+    )
+}
+
+#' user_manager Server Functions
+#'
+#' @noRd
+mod_user_manager_server <- function(id, user_iniciado) {
+    moduleServer(id, function(input, output, session) {
+        ns <- session$ns
+
+        bs4Dash::updateBox("box_manager", "remove")
+
+        out <- reactiveValues(
+            save_sucess = 0
+        )
+
+        reset_ui <- function() {
+            updateTextInput(session, "user_id", value = "")
+            updateTextInput(session, "name", value = "")
+            updateTextInput(session, "last_name", value = "")
+            updateSelectInput(session, "privileges", selected = "user1")
+            updateSelectInput(session, "responds_to", choices = user_get_from_privileges("user2"))
+        }
+
+        new_user_data <- reactive({
+            data.frame(
+                user_id = input$user_id,
+                name = input$name,
+                last_name = input$last_name,
+                privileges = input$privileges,
+                responds_to = input$responds_to,
+                date_added = as.character(input$date_added)
+            )
+        })
+
+        observe({
+            if (isFalsy(input$user_id, input$name, input$last_name)) {
+                alert_error(session, "Debe especificar datos completos de usuario")
+            } else {
+                user_insert(new_user_data())
+
+                reset_ui()
+
+                out$save_success <- out$save_success + 1
+
+                bs4Dash::updateBox(id = "box_manager", action = "remove")
+                alert_info(session = session, sprintf("Se añadió al usuario %s", input$user_id))
+            }
+        }) |> bindEvent(input$save)
+
+        observe({
+            bs4Dash::updateBox(id = "box_manager", action = "restore")
+        }) |> bindEvent(input$add)
+
+        observe({
+            reset_ui()
+            bs4Dash::updateBox(id = "box_manager", action = "remove")
+
+        }) |> bindEvent(input$cancelar)
+
+        # return value
+        reactive(out$save_success)
+
+    })
+}
+
+## To be copied in the UI
+# mod_user_manager_ui("user_manager_1")
+
+## To be copied in the server
+# mod_user_manager_server("user_manager_1")
+
+
+mod_user_manager_apptest <- function(user_iniciado = "dgco93@mininter.gob.pe") {
+    ui <- tagList(
+        bs4Dash::dashboardPage(
+            header = bs4Dash::dashboardHeader(title = "TEST"),
+            sidebar = bs4Dash::dashboardSidebar(
+                bs4Dash::sidebarMenu(
+                    bs4Dash::menuItem(text = "Test",
+                                      tabName = "tasks",
+                                      icon = icon("user"))
+                )
+            ),
+            body = bs4Dash::dashboardBody(
+                bs4Dash::tabItem(
+                    tabName = "tasks",
+                    mod_user_manager_ui("test", user_iniciado),
+                    tags$hr(),
+                    mod_user_manager_output("test", user_iniciado)
+                )
+            )
+        )
+    )
+
+    server <- function(input, output, session) {
+        mod_user_manager_server("test", user_iniciado)
+    }
+
+    shinyApp(ui, server)
+}
--- R/mod_users.R
+++ R/mod_users.R
@@ -10,16 +10,13 @@
 mod_users_ui <- function(id) {
   ns <- NS(id)
   tagList(
-      mod_user_manager_ui(ns("user_manager")),
-      btn_trash(ns("delete_user")),
+      mod_user_manager_ui(ns("user_manager"), user_iniciado),
+      btn_eliminar(ns("delete_user"), icon = icon("trash")),
+      tags$hr(),
+      mod_user_manager_output(ns("user_manager"), user_iniciado),
     bs4Dash::box(
       title = "Usuarios registrados",
       width = 12,
-      # sidebar = bs4Dash::boxSidebar(
-      #   id = ns("sidebar"),
-      #   width = 25,
-      #   btn_add(ns("add"))
-      # ),
       DT::DTOutput(ns("tabla"))
     )
   )
@@ -33,63 +30,18 @@ mod_users_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
 
+    btn_usuario_agregado <- mod_user_manager_server("user_manager")
+
     vals <- reactiveValues(
       data_users = user_get_all()
     )
-
-    # new_user_data <- reactive({
-    #   data.frame(
-    #     user_id = input$user_id,
-    #     name = input$name,
-    #     last_name = input$last_name,
-    #     privileges = input$privileges,
-    #     responds_to = input$responds_to,
-    #     date_added = as.character(input$date_added)
-    #   )
-    # })
 
     user_for_deleting <- reactive({
       vals$data_users$user_id[input$tabla_rows_selected]
     })
 
-    # observeEvent(input$add, {
-    #   showModal(modalDialog(
-    #     title = "Nuevo usuario",
-    #
-    #     textInput(ns("user_id"), "ID"),
-    #     textInput(ns("name"), "Nombres"),
-    #     textInput(ns("last_name"), "Apellidos"),
-    #     selectInput(ns("privileges"), "Privilegios", choices = c("user1", "user2", "admin")),
-    #     selectInput(ns("responds_to"), "Responde a:", choices = user_get_from_privileges("user2")),
-    #     dateInput(ns("date_added"), "Fecha", language = "es", value = lubridate::today("America/Lima")),
-    #
-    #     footer = tagList(
-    #       modalButton("Cancelar"),
-    #       btn_agregar(ns("insert_user"))
-    #     )
-    #   ))
-    # })
-
-    # observeEvent(input$insert_user, {
-    #
-    #   user_insert(new_user_data())
-    #
-    #   updateTextInput(session, "user_id", value = "")
-    #   updateTextInput(session, "name", value = "")
-    #   updateTextInput(session, "last_name", value = "")
-    #   updateSelectInput(session, "privileges", selected = "user1")
-    #   updateSelectInput(session, "responds_to", choices = user_get_from_privileges("user2"))
-    #
-    #   vals$data_users <- user_get_all()
-    #
-    #   removeModal()
-    #
-    #   alert_info(session = session, sprintf("Se añadió al usuario %s", input$user_id))
-    #
-    # })
-
     observeEvent(input$delete_user,{
-        if (isTruthy(user_for_deleting())) {
+        if (!isTruthy(user_for_deleting())) {
             alert_error(session, "Debe seleccionar usuario a eliminar")
         } else {
             user_remove(user_for_deleting())
@@ -97,6 +49,12 @@ mod_users_server <- function(id){
             vals$data_users <- user_get_all()
         }
     })
+
+    observe({
+        vals$data_users <- user_get_all()
+    }) |> bindEvent(btn_usuario_agregado())
+
+    observe(print(user_for_deleting())) |> bindEvent(input$tabla_rows_selected)
 
     output$tabla <- DT::renderDT(
       expr = vals$data_users,
