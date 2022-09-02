865f0634dcf3e9ad8299fdada4e773de5557b6f6
Samuel Calderon <samuel.calderon@uarm.pe>
2022-09-02 16:53:06 -0500

    divide db methods

--- R/SessionData.R
+++ R/SessionData.R
@@ -1,56 +1,77 @@
 SessionData <- R6::R6Class(
     classname = "SessionData",
     public = list(
-        con = NULL,
-        user_iniciado = NULL,
+        user_id = NULL,
         user_names = NULL,
         privileges = NULL,
         groups = NULL,
         tasks = NULL,
-        templates = NULL,
-        db_get_query = function(...) {
-            dots <- list(...)
-            dots[[".con"]] <- self$con
-            query <- do.call(what = glue::glue_sql, args = dots)
-            
-            DBI::dbGetQuery(self$con, query)
-        },
-        db_execute_statement = function(...) {
-            dots <- list(...)
-            dots[[".con"]] <- self$con
-            
-            statement <- do.call(what = glue::glue_sql, args = dots)
-            
-            DBI::dbExecute(self$con, statement)
-        }
+        templates = NULL
+    ),
+    private = list(
+        con = NULL
+        # db_get_query = NULL,
+        # db_execute_statement = NULL
     )
 )
 
-SessionData$set("public", "initialize", function(user_iniciado,
+SessionData$set("private", "db_get_query", function(...) {
+    dots <- list(...)
+    dots[[".con"]] <- private$con
+    query <- do.call(what = glue::glue_sql, args = dots)
+    
+    DBI::dbGetQuery(private$con, query)
+})
+
+SessionData$set("private", "db_execute_statement", function(...) {
+    dots <- list(...)
+    dots[[".con"]] <- private$con
+    
+    statement <- do.call(what = glue::glue_sql, args = dots)
+    
+    DBI::dbExecute(private$con, statement)
+})
+
+SessionData$set("private", "db_connect_remote", function() {
+    DBI::dbConnect(
+        drv = RMariaDB::MariaDB(),
+        user = Sys.getenv("DB_USER"),
+        password = Sys.getenv("DB_SECRET"),
+        dbname = Sys.getenv("DB_NAME"),
+        host = Sys.getenv("DB_HOST"),
+        port = Sys.getenv("DB_PORT")
+    )
+})
+
+SessionData$set("private", "db_connect_local", function(){
+    DBI::dbConnect(
+        drv = RSQLite::SQLite(),
+        dbname = Sys.getenv("DB_NAME")
+    )
+})
+
+SessionData$set("private", "get_privileges", function() {
+    private$db_get_query("SELECT privileges
+                              FROM users
+                              WHERE (user_id = {self$user_id})")$privileges
+})
+
+
+SessionData$set("public", "initialize", function(user_id,
                                                  remote = getOption("reporte_avance.remote")) {
     message("Starting SessionData")
-    self$user_iniciado <- user_iniciado
-    self$con <- if (remote) {
-        DBI::dbConnect(
-            drv = RMariaDB::MariaDB(),
-            user = Sys.getenv("DB_USER"),
-            password = Sys.getenv("DB_SECRET"),
-            dbname = Sys.getenv("DB_NAME"),
-            host = Sys.getenv("DB_HOST"),
-            port = Sys.getenv("DB_PORT")
-        )
-    } else {
-        DBI::dbConnect(drv = RSQLite::SQLite(),
-                       dbname = Sys.getenv("DB_NAME"))
-    }
-    self$privileges <- self$db_get_query("SELECT privileges
-                              FROM users
-                              WHERE (user_id = {self$user_iniciado})")$privileges
+    
+    private$con <- if (remote) private$db_connect_remote() else private$db_connect_local()
+    self$user_id <- user_id
+    self$privileges <- private$get_privileges()
+    self$groups <- self$groups_compute()
+    self$tasks <- NULL
+    self$templates <- NULL
 })
 
 SessionData$set("public", "finalize", function() {
     message("Cleaning up SessionData")
-    DBI::dbDisconnect(self$con)
+    DBI::dbDisconnect(private$con)
 })
 
 # test <- SessionData$new("dgco93@mininter.gob.pe")
