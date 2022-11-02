d536a81047ab8cb2c6aa14075b75a4ac8592b0dc
Samuel Calderon <samuel.calderon@uarm.pe>
2022-11-02 15:36:21 -0500

    use query constructor inside get_tasks()

--- R/DBData.R
+++ R/DBData.R
@@ -119,7 +119,7 @@ DBData <- R6::R6Class(
                 )
         },
         get_tasks = function() {
-            query <- 
+            query_tasks <- super$db_make_query(
                 "SELECT *
                 FROM tasks 
                 WHERE org_id IN ({orgs*}) AND
@@ -128,11 +128,25 @@ DBData <- R6::R6Class(
                             status_current = 'Terminado' AND
                             time_last_modified BETWEEN date_sub(now(), INTERVAL 1 WEEK) AND now()
                             )
-                        )"
+                        )",
+                orgs = names(self$orgs),
+                groups = names(self$groups)
+            )
             
-            db_data <- super$db_get_query(query, 
-                                          orgs = names(self$orgs),
-                                          groups = names(self$groups))
+            db_data <- super$db_get_query(
+                "SELECT 
+                    lhs.*,
+                    rhs.name AS assignee_name,
+                    rhs.last_name AS assignee_last_name,
+                    rhs2.name AS assigned_by_name,
+                    rhs2.last_name AS assigned_by_last_name
+                FROM ({query_tasks}) AS lhs
+                LEFT JOIN users rhs ON
+                    lhs.assignee = rhs.user_id
+                LEFT JOIN users rhs2 ON
+                    lhs.assigned_by = rhs2.user_id",
+                query_tasks = query_tasks
+            ) 
             
             db_data |> 
                 purrr::pmap(list) |> 
