10fe4f8d648e14de3436691a26cb65b1f65b2f34
Samuel Calderon <samuel.calderon@uarm.pe>
2022-11-04 15:39:11 -0500

    orgs can now be initialized arbitrarily

--- R/class_Orgs.R
+++ R/class_Orgs.R
@@ -6,8 +6,32 @@ Organisation <- R6::R6Class(
         org_users = NULL,
         initialize = function(email) {
             super$initialize(email)
-            self$orgs <- private$get_orgs()
-            self$org_users <- private$get_org_users()
+            private$sync_orgs()
+        },
+        org_initialize = function() {
+            org_id <- private$org_create()
+            
+            self$org_add_user(
+                org_id = org_id,
+                user_id = self$user$user_id,
+                role = "owner"
+            )
+            private$sync_orgs()
+        },
+        org_add_user = function(org_id, user_id, role) {
+            t_stamp <- super$get_timestamp()
+            statement <- 
+                "INSERT INTO org_users
+                SET
+                    org_id = {org_id},
+                    user_id = {user_id},
+                    role = {role},
+                    time_creation = {t_stamp},
+                    time_last_modofied = {t_stamp}"
+            
+            super$db_execute_statement(statement, .envir = rlang::current_env())
+            
+            private$sync_org_users()
         }
     ),
     private = list(
@@ -54,6 +78,37 @@ Organisation <- R6::R6Class(
                     ~purrr::pmap(.x, list) |> 
                         setNames(.x$user_id)
                 )
+        },
+        org_create = function() {
+            id <- ids::random_id()
+            t_stamp <- super$get_timestamp()
+            
+            statement <- 
+                "INSERT INTO organisations
+                SET
+                    org_id = {id},
+                    org_title = 'Organización sin nombre',
+                    org_description = '',
+                    time_creation = {t_stamp},
+                    time_last_modified = {t_stamp}"
+            super$db_execute_statement(statement, .envir = rlang::current_env())
+            
+            return(id)
+        },
+        org_delete = function(org_id) {
+            statement <- 
+                "DELETE FROM organisations
+                WHERE org_id = {org_id}"
+            
+            super$db_execute_statement(statement, .envir = rlang::current_env())
+            private$sync_orgs()
+        },
+        sync_orgs = function() {
+            self$orgs <- private$get_orgs()
+            self$org_users <- private$get_org_users()
+        },
+        sync_org_users = function() {
+            self$org_users <- private$get_org_users()
         }
     )
 )
