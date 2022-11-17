3770db0cf1ac53f05de085b8b2b5a90f548ef5f0
calderonsamuel <samuel.calderon@uarm.pe>
2022-11-17 14:14:51 -0500

    use reportesAPI

--- R/app_server.R
+++ R/app_server.R
@@ -3,6 +3,7 @@
 #' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
 #' @import shiny
+#' @importFrom reportesAPI AppData
 #' @noRd
 app_server <- function(input, output, session) {
     # Your application server logic
--- R/class_AppData.R
+++
@@ -1,9 +0,0 @@
-AppData <- R6::R6Class(
-    classname = "AppData",
-    inherit = Task,
-    public = list(
-        initialize = function(email) {
-            super$initialize(email)
-        }
-    )
-)
