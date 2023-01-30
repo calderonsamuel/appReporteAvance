c2554ddbc0a179a73e46a7eb3ddc1e700c73fa57
Samuel Calderon <samuel.calderon@uarm.pe>
2023-01-30 10:04:32 -0500

    input needs manually adding the js file

--- R/textInputWithMaxLength.R
+++ R/textInputWithMaxLength.R
@@ -24,13 +24,16 @@ textInputWithMaxLength <- function(inputId, label, value = "", width = NULL,
     
 }
 
-tagList(
-    textInputWithMaxLength("hi", "hi", maxlength = 25, maxlengthCounter = TRUE),
-    htmltools::htmlDependency(
-        name = "maxlength", 
-        version = "0.1.0",
-        src = "inst/js",
-        script = "maxlength.js"
-    )
-) |> 
-    bslib::page_fluid()
+bslib::page_fluid(
+    tags$head(
+        # htmltools::htmlDependency(
+        #     name = "maxlength",
+        #     version = "0.1.0",
+        #     package = "appReporteAvance",
+        #     src = "js",
+        #     script = "maxlength.js"
+        # )
+        includeScript("inst/js/maxlength.js")
+    ),
+    textInputWithMaxLength("hi", "hi", maxlength = 25, maxlengthCounter = TRUE)
+)
--- inst/js/maxlength.js
+++ inst/js/maxlength.js
@@ -1,9 +1,22 @@
 // works when copied and pasted in browser console. needs a document.onload()
 
-let textInput = document.getElementById("hi")
-let counter = document.getElementById("hi-mlCounter")
-// https://stackabuse.com/character-counter-for-text-areas-with-vanilla-javascript/
+function textInputCounter(elementId) {
+    let textInput = document.getElementById(elementId)
+    let counter = document.getElementById(elementId + "-mlCounter")
+    // https://stackabuse.com/character-counter-for-text-areas-with-vanilla-javascript/
+    
+    textInput.addEventListener('keydown', () => {
+        counter.innerText = textInput.value.length + "/" + textInput.attributes.maxlength.value
+    })
+}
 
-textInput.addEventListener('keydown', () => {
-    counter.innerText = textInput.value.length + "/" + textInput.attributes.maxlength.value
-})
+console.log('hi external')
+
+window.onload = (event) => {
+    textInputCounter('hi')
+    console.log('hi internal')
+};
+
+// textInputCounter('hi')
+
+// document.onload = (event) => textInputCounter('hi')
