c555f106d8df5906f8f7b042ed43ecd73074c854
calderonsamuel <samuel.calderon@uarm.pe>
2023-03-17 16:28:26 -0500

    add info about units

--- R/report_box.R
+++ R/report_box.R
@@ -1,10 +1,22 @@
 report_box <- function(report, ns = NULL) {
     id <- ns_safe(report$report_id, ns)
     
+    day_reported <- format(report$time_reported, "%d %b")
+    time_reported <- format(report$time_reported, "%H:%M:%S")
+    
     bs4Dash::box(
         title = report$report_title,
         width = 12,
         report$details,
-        format(report$time_reported, "%d %b %H:%M:%S")
+        
+        lapply(report$quantities, \(x){
+            div(
+                glue::glue("{output_unit}: {output_progress}", .envir = x)
+            )
+        }),
+        
+        glue::glue("Reportado el {day_reported} a las {time_reported}")
+        
+        
     )
 }
