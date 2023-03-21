0a04302807d06c319e145ad679db1d5a07283113
calderonsamuel <samuel.calderon@uarm.pe>
2023-03-21 16:03:19 -0500

    update for pkg building

--- R/app_server.R
+++ R/app_server.R
@@ -3,7 +3,6 @@
 #' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
 #' @import shiny
-#' @importFrom reportesAPI AppData
 #' @noRd
 app_server <- function(input, output, session) {
     # Your application server logic
