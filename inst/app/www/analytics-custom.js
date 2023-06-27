a2e8159656040e79006b4301b29e9859377483d8
Samuel Calderon <samuel.calderon@uarm.pe>
2023-06-27 12:23:13 -0500

    close #173

---
+++ inst/app/www/analytics-custom.js
@@ -0,0 +1,16 @@
+$(() => {
+    
+  // The first part is loaded from "inst/app/www" in "R/app_ui.R"
+
+  /* Default installation */
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  };
+  gtag('js', new Date());
+  gtag('config', 'G-7P45QNH8JV');
+  
+  /* Custom */
+
+});
